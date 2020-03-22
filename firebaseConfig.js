786aa9067e049d9b21f61e50e0d44b75bcf69241
Gabriel Ward <gabriel.ward@re-leased.com>
2020-03-22 18:27:38 +1300

    initial commit

---
+++ firebaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyAdbAuIvvWbPiXFkewvkdMzBvaVpkQBqLg",
+    authDomain: "vue-calendar-62df1.firebaseapp.com",
+    databaseURL: "https://vue-calendar-62df1.firebaseio.com",
+    projectId: "vue-calendar-62df1",
+    storageBucket: "vue-calendar-62df1.appspot.com",
+    messagingSenderId: "104149077391",
+    appId: "1:104149077391:web:4d509d6e0e0b1cf5614ffa"
+  };
--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify';
+import VueTextareaAutosize from 'vue-textarea-autosize';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import firebaseConfig from '../firebaseConfig';
+
+Vue.use(VueTextareaAutosize);
 
 Vue.config.productionTip = false
 
+firebase.initializeApp(firebaseConfig);
+
+export const db = firebase.firestore();
+
 new Vue({
-  render: h => h(App),
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
